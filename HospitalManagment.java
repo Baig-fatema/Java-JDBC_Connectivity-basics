58f2d23b470e6651c2981747569ceec84d096fe8
Baig-fatema <167601810+Baig-fatema@users.noreply.github.com>
2025-04-10 12:54:03 +0530

    Update HospitalManagment.java

--- HospitalManagment.java
+++ HospitalManagment.java
@@ -1,3 +1,4 @@
+
 package JDBC.HospitalManagment;
 
 import java.sql.Connection;
@@ -5,6 +6,8 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.Scanner;
+
+
 
 //main class
 public class HospitalManagment {
@@ -31,7 +34,8 @@ public class HospitalManagment {
                 System.out.println("2.View Patients.");
                 System.out.println("3.View Doctor's.");
                 System.out.println("4.Book Appoinment.");
-                System.out.println("5.Exit.");
+                System.out.println("5.view Appoinment.");
+                System.out.println("6.Exit.");
                 System.out.println("Enter You Choice.");
                 int choice = sc.nextInt();
                 switch (choice) {
@@ -53,6 +57,11 @@ public class HospitalManagment {
                         System.out.println();
                         break;
                     case 5:
+                        // book Appoinment
+                        ViewAppoinment(con, sc);
+                        System.out.println();
+                        break;    
+                    case 6:
                     System.out.println("Thank you For Using Hospital managment System.");
                     System.out.print("Exiting System");
                     Exit e = new Exit();
@@ -91,6 +100,58 @@ public class HospitalManagment {
             e.printStackTrace();
         }
         return false;
+    }
+    //view appoinment
+    public static void ViewAppoinment(Connection con, Scanner sc) {
+    	System.out.println("*********** Patients appoinment: ******************");
+    	System.out.println("Enter your Patient id: ");
+    	int pid=sc.nextInt();
+    	String docNameString="";
+    	String patiensNameString="";
+    	String queryString="SELECT * from appoinments WHERE patient_id=? ;";
+    	try {
+    		PreparedStatement pdPreparedStatement=con.prepareStatement(queryString);
+        	pdPreparedStatement.setInt(1, pid);
+        	ResultSet rSet=pdPreparedStatement.executeQuery();
+
+        	while(rSet.next()) {
+        		int docId=rSet.getInt("doctor_id");
+        		String dateString=rSet.getString("appoinment_date");
+        		String qString="SELECT name FROM doctors WHERE id=? ;";
+        		String PatqString="SELECT name FROM patients WHERE id=? ;";
+        		try {
+        			PreparedStatement pdStatement=con.prepareStatement(qString);
+        			pdStatement.setInt(1, docId);
+        			ResultSet rsSet=pdStatement.executeQuery();
+        			if(rsSet.next()) {
+        			 docNameString=rsSet.getString("name");
+        			}
+        		}catch (Exception e) {
+					// TODO: handle exception
+        		    e.printStackTrace();
+				}
+        		try {
+        			PreparedStatement pdStatement=con.prepareStatement(PatqString);
+        			pdStatement.setInt(1, pid);
+        			ResultSet rsSet=pdStatement.executeQuery();
+        			if(rsSet.next()) {
+        			 patiensNameString=rsSet.getString("name");
+        			}
+        		}catch (Exception e) {
+					// TODO: handle exception
+        		    e.printStackTrace();
+				}
+        		System.out.println("Patients Name: "+patiensNameString);
+                System.out.println("Doctors Name: "+docNameString);
+                System.out.println("Appoinment Date: "+dateString);
+        	}
+        	
+        	
+    	}catch (Exception e) {
+			// TODO: handle exception
+  
+    		e.printStackTrace();
+		}
     }
 
     // book appoinment
